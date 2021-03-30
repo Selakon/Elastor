1d2d3094fc1785231969f31e279826f392e4f6e1
Selakon <gamedev@tonipastor.esa>
2021-03-30 13:52:34 +0200

    modified player movement values

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,31 +1,29 @@
 extends KinematicBody2D
 
-export var h_speed = 600
+export var h_speed = 500
 export var gravity = Vector2(0,50)
 export var jump_speed = Vector2(0,-600)
 export var max_speed = 450
 
 var velocity = Vector2(0,0)
-var horizontal_movement_dir
+var horizontal_movement_dir = 0
 
 func _physics_process(delta):
+	
 	# horizontal movement
 	var left = Input.is_action_pressed("ui_left")
 	var right = Input.is_action_pressed("ui_right")
 	
+	horizontal_movement_dir = -int(left) + int(right) 
+	
 	if left and right:
 		velocity.x = 0
 	elif left:
-		velocity.x += h_speed * delta * horizontal_movement_dir
+		velocity.x += h_speed * horizontal_movement_dir * delta
 	elif right: 
-		velocity.x += h_speed * delta * horizontal_movement_dir
+		velocity.x += h_speed * horizontal_movement_dir * delta
 	else: 
 		velocity.x = 0 
-	if(left):
-		print("left: ", left) 
-	if(right):
-		print("right: ", right) 
-	horizontal_movement_dir = -int(left) + int(right) 
 
 	# jump  
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
@@ -34,5 +32,6 @@ func _physics_process(delta):
 	# apply gravity 
 	if !is_on_floor():
 		velocity += gravity 
-	
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	
